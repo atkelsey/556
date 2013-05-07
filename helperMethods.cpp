33becb981b57101d70c00f08c2245c30af02df97
atkelsey <atkelsey@wisc.edu>
2013-05-07 17:44:40 -0500

    I dont hhhhknow

--- helperMethods.cpp
+++ helperMethods.cpp
@@ -127,7 +127,7 @@
 				 }
 				 else {
 					 //cout << "BOOM \n";
-					 netWeight = rst->cap;
+					 netWeight = (rst->edgeUtils[tempEdge]/ rst->cap);
 					 rst->nets[i].weight = netWeight;
 				 }
 				 if (rst->edgeUtils[tempEdge] > rst->edgeCaps[tempEdge]){
